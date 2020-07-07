3ebb97453c8b62d1e6ac1155b550eb1332d0ac8d
Calvin <calvinmorett@users.noreply.github.com>
2020-07-07 00:46:57 -0400

    Add files via upload

--- givestacks.py
+++ givestacks.py
@@ -1,13 +1,45 @@
 import math
 
 jtype = input("Type: cutdown | or |  default? ") # jobtype - (125 stacks) or (250 stacks)
-#print(jtype)
 itns = input("what is the total number of sheets? ") # total number sheets
 tns = int(itns)
-#print(tns)
+itln = input("what is the total number of labels? ") # total label number
+tln = int(itln)
 
-#tln = input("what is the total number of labels? ") # total label number
-#lps = input("how many can you fit per sheet? ") # labels per sheet
+stacksize = 0
+
+def givestacks(stacksize):
+    sumofjtype = tns/stacksize
+    trunc_sum = math.trunc(sumofjtype)
+    subfromtotal = trunc_sum * stacksize
+	
+    if tln > 10: # total labels per sheet
+        leftovercheck = stacksize - 15
+    else:
+        leftovercheck = stacksize - 10
+
+    leftover = tns - subfromtotal
+	
+    if leftover < leftovercheck:
+        print(str(trunc_sum) + ' stacks of ' + str(stacksize) + ' with ' + str(leftover) + ' as leftover.')
+    else:
+        new_tsum = trunc_sum + 1
+        print(str(new_tsum) + ' stacks of ' + str(stacksize))
+
+# print: {trunc_sum} stacks of {jtype(stacksize)} with a stack of {leftover}
+
+if jtype == "default":
+    stacksize = 250;
+    print('250 Stacks for ' + str(tns))
+    print('')
+    givestacks(250)
+elif jtype == "cutdown":
+    stacksize = 125;
+    print('125 Stacks for ' + str(tns))
+    print('')
+    givestacks(125)
+else: 
+    print('You must write |cutdown| or |default| for Type')
 
 # {tns}1234 cd
 # 1234 % 125{cd}
@@ -39,39 +71,8 @@ tns = int(itns)
 # print: {trunc_sum} stacks of {jtype} with a stack of {leftover}
 
 # total number % jtype
+#######################
 
-stacksize = 0;
-
-
-def givestacks(stacksize):
-    sumofjtype = tns/stacksize
-    # print(sumofjtype)
-    # print('sum of jtype')
-    trunc_sum = math.trunc(sumofjtype)
-    # print(trunc_sum)
-    # print('trunc sum')
-    subfromtotal = trunc_sum * stacksize
-    # print(subfromtotal)    
-    # print('sub total')
-    leftovercheck = stacksize - 10
-    # print(leftovercheck)
-    # print('stack check')
-    leftover = tns - subfromtotal
-    # print(leftover)    
-    # print('leftoverzzz')
-    if leftover < leftovercheck:
-        print(str(trunc_sum) + ' stacks of ' + str(stacksize) + ' with ' + str(leftover) + ' as leftover.')
-    else:
-        new_tsum = trunc_sum + 1
-        print(str(new_tsum) + ' stacks of ' + str(stacksize))
-
-# print: {trunc_sum} stacks of {jtype(stacksize)} with a stack of {leftover}
-
-if jtype == "default":
-    stacksize = 250;
-    print('250 Stacks for ' + str(tns))
-    print('')
-    givestacks(250)
     ##  use math.trunc() to
     # take everything before decimal point 
     # e.g. 5.29 = 5
@@ -84,31 +85,3 @@ if jtype == "default":
     # if addtoTotal is < 20 jtype just make it another stack 
     # add 1 to precalc
     # & print(str(precalc) + ' stacks)
-elif jtype == "cutdown":
-    stacksize = 125;
-    print('125 Stacks for ' + str(tns))
-    print('')
-    givestacks(125)
-else: 
-    print('You must write |cutdown| or |default| for Type')
-
-# e.g.
-# tns = 520 sheets example
-# default label
-# 
-# 520 % default label
-# = 2.08
-# 250 x 250
-# + .08 = 20
-# ____________
-
-
-# e.g.
-# tns = 1234 sheets example
-# cutdown
-# 
-# 1234 % cutdown
-# 9.874
-# 9(125) = 1125
-# .874 = 109
-# ____________
